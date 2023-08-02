dd49706573c4ece9bf523d96f3e72dfc801dfd0e
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-02 20:18:07 +0300

    Footer: Temporary footer is created

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="App">
+      <hr />
+      <h1>NASA</h1>
+    </div>
+  );
+}
+
+export default Footer;
