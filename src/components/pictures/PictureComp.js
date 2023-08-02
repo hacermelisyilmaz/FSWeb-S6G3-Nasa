759350bb4692c92662bcd53e3fe0894c08ef325f
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-02 22:26:09 +0300

    PictureComp: Each picture component is created

---
+++ src/components/pictures/PictureComp.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function PictureComp(props) {
+  const { pictureData } = props;
+
+  const style = {
+    backgroundImage: `url(${pictureData.hdurl})`,
+    backgroundSize: "cover",
+  };
+
+  return (
+    <div className="PictureComp" style={style}>
+      <h2 className="comp-title">{pictureData.title}</h2>
+      <h3 className="comp-date">{pictureData.date}</h3>
+    </div>
+  );
+}
+
+export default PictureComp;
