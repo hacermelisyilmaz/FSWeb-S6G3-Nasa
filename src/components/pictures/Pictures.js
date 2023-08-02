8374749943970d2b8bca3c421a766832681fc2d9
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-02 22:48:59 +0300

    Pictures: State variable is added and variable names are changed accordingly. Key is added to JSX element inside map()

--- src/components/pictures/Pictures.js
+++ src/components/pictures/Pictures.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import PictureComp from "./PictureComp.js";
 
 function Pictures(props) {
   const { data } = props;
-  //console.log("Pictures: ", data);
+  const [pictureData, setPictureData] = useState(data);
   return (
     <div className="Pictures flex-container">
-      {data.map((picData) => {
+      {pictureData.map((picData) => {
         if (!picData.hdurl) {
           picData.hdurl = picData["thumbnail_url"];
         }
-        return <PictureComp pictureData={picData} />;
+        return <PictureComp key={picData.id} pictureData={picData} />;
       })}
     </div>
   );
