657980497f5d314adf140f988707f313a496a1b7
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-02 20:17:51 +0300

    Header: Temporary header is created

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import "./Header.css";
+
+function Header() {
+  return (
+    <div className="App">
+      <h1>Astronomy Picture of the Day</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default Header;
