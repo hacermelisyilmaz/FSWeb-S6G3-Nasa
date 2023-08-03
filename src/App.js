547df360c35e57f98408f66c7fc02fb6913d1b1d
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-04 00:31:20 +0300

    Additional styling, changed background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Main from "./components/Main.js";
 import "./App.css";
 
 function App() {
+  const [apodData, setApodData] = useState();
+  const [imgOfDay, setImgOfDay] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=WJnS0fGtrkgAxSmM3KEbPn7PwROQPg79530w3J62"
+      )
+      .then(function (response) {
+        setApodData(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
+      .finally(function () {
+        console.log("Axios get request has been made");
+      });
+  }, []);
+
+  useEffect(() => {
+    apodData && setImgOfDay(apodData.hdurl);
+  }, [apodData]);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${imgOfDay})`,
+      }}
+    >
       <div className="App-Cover">
         <img
           className="App-logo"
@@ -13,7 +42,7 @@ function App() {
         <h1>Astronomy Picture of the Day</h1>
       </div>
       <div className="App-Main flex-container">
-        <Main />
+        <Main apodData={apodData} />
       </div>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import MainLeft from "./MainLeft.js";
 import MainPicture from "./MainPicture.js";
 import MainRight from "./MainRight.js";
 
-function Main() {
-  const [apodData, setApodData] = useState();
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.nasa.gov/planetary/apod?api_key=WJnS0fGtrkgAxSmM3KEbPn7PwROQPg79530w3J62"
-      )
-      .then(function (response) {
-        setApodData(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      })
-      .finally(function () {
-        console.log("Axios get request has been made");
-      });
-  }, []);
-
+function Main({ apodData }) {
   return (
     <div className="Main flex-container">
       <MainLeft pOD={apodData} />
